85cd1441b2a4557759307223db7e588229c16446
SrikanthGR18 <grsrikanth5@gmail.com>
2025-08-09 08:04:58 -0400

    Revert "added another line by <your-username>"
    This reverts commit ea1eec029a1337992ea9cd9b52377337ae5ec463.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,7 +10,7 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update By SrikanthGR18
- # Here is another update by SrikanthGR18
+
 
 
 
